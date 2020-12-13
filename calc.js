81f5dec8281ba98e35a3712dbf9df9b63a815af1
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2020-12-13 13:38:56 -0300

    desistindo

---
+++ calc.js
@@ -0,0 +1,10 @@
+var factor = document.querySelector('#factor')
+let btncalc = document.querySelector('#btncalc')
+
+function tabuada() {
+    var count=1
+    while (count<=10) {
+        let x = factor*count
+        console.log (x);
+}
+}
